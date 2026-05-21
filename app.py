0ea30f216d58b1778af7b10ebf4cf89bbe5152ec
Rachin02 <rachin15-5466@diu.edu.bd>
2026-05-21 16:00:21 +0600

    bangla chatbot with streamlit with system prompt

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ from langchain_core.output_parsers import StrOutputParser
 # Langsmith tracking
 os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
 os.environ["LANGSMITH_PROJECT"] = "Bangla Chatbot"
-os.environ["LANGSMITH_TRACKING"] = "true"
+os.environ["LANGSMITH_TRACING"] = "true"
 
 # prompt
 prompt = ChatPromptTemplate.from_messages(
   [
-   ("system", "You are a helpful assistant. answer the following question simply"),
+   ("system", "You are a helpful assistant. answer the following question simply. remember question will be in english, banglish and bangla language but you will always replay in bangla languaeg."),
     ("user", "Question: {question}")
   ]
  )
@@ -27,19 +27,27 @@ def generate_answer(question, model, token):
     parser = StrOutputParser()
     chain = prompt | model | parser
 
-    response = chain.invoke({"question": question})
-    return response
+    # response = chain.invoke({"question": question})
+    # return response
+    return chain.stream({"question":question})
 
 # Title of the web
 st.title("Bangla Chatbot")
 
 # sidebar for setting
-model =  st.sidebar.selectbox("Select GPT model", ["gpt-4o-mini", "gpt-5-nano", "gpt-5.4-nano"])
+model =  st.sidebar.selectbox("Select GPT model", ["gpt-4o-mini", "gpt-5.4-nano", "gpt-5-nano"])
 
 # output token lenght define
-token = st.sidebar.slider("Select output respons length in tokens", min_value= 30, max_value= 200, value = 100)
+token = st.sidebar.slider("Select output response length in tokens", min_value= 30, max_value= 300, value = 100)
 
 # inferface for user input
-# st.write("Ask me anything...")
 question = st.text_input("Ask me anything: ")
-st.write(question)
+
+# answer
+if st.button("Ask"):
+    if question:
+        answer = generate_answer(question, model, token)
+        st.write_stream(answer)
+    else:
+        st.write("ask question")
+
