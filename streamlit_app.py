f7696c1c683abb66606edcf82a0507cb249e5fb6
Rachin02 <rachin15-5466@diu.edu.bd>
2026-05-27 22:15:33 +0600

    better version with chat UI

---
+++ streamlit_app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Store history
+if "store" not in st.session_state:
+    st.session_state.store = {}
+store = st.session_state.store
+
+# Chat UI messages store
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role":"assistant", "content":"Hi! how can i help you"}]
+
+
+st.set_page_config(page_title="Bangla Chatbot", page_icon= "🌍")
+st.title("Bangla Chatbot")
+
+model_name = st.sidebar.selectbox("Choose GPT model",["gpt-4o-mini","gpt-5-nano"])
+token_len = st.sidebar.slider("Select maximum output token length", min_value= 50, max_value= 300, value = 100)
+st.sidebar.markdown(
+    """
+        <div style = " background-color: #8dc6ff;
+                       color : black;
+                       padding : 7px;
+                       border-radius: 10px;
+                       text-align: center;
+                       font-size:13px;
+                       font-weight:200;"
+        >Developed by Rachin
+        </div>
+    """,
+    unsafe_allow_html= True
+)
+
+
+llm_model = ChatOpenAI(model = model_name, max_completion_tokens= token_len, streaming = True)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system","You are a helpful assistant. answer the following question simply. remember question will be in english, banglish and bangla language but you will always replay in bangla language. don't use word from any other language except Bangla"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user","{question}")
+])
+
+chain = prompt | llm_model 
+
+def get_session_history(session_id:str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+chain_with_history = RunnableWithMessageHistory(chain, get_session_history, input_messages_key="question", history_messages_key="chat_history")
+
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+
+question = st.chat_input("Ask Anything")
+
